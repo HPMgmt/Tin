1acdcae980d65f9e698235d396c1233ed33e6970
HPMgmt <127541166+HPMgmt@users.noreply.github.com>
2023-04-16 16:44:45 -0400

    Add files via upload
    Tin1

---
+++ navbar.py
@@ -0,0 +1,18 @@
+<body>
+<h3>Random Number: {{ num }}</h3>
+<nav class="navbar-expand-lg bg-light">
+    <ul class="navbar-nav">
+        <li class="nav-item"
+            <a class="nav-link" href="https://www.appbrewery.co/">Email</a>
+        </li>
+        <li class="nav-item"
+            <a class="nav-link" href="https://www.appbrewery.co/">Call</a>
+        </li>
+        <li class="nav-item"
+            <a class="nav-link" href="https://www.appbrewery.co/">Address</a>
+        </li>
+
+    </ul>
+</nav>
+
+
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import random
+from datetime import date
+import requests
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    random_number = random.randint(1, 100)
+    current_year = (datetime.datetime.now().year)
+    return render_template("index.html", num=random_number, year=current_year)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
